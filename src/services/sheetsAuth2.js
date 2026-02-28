bca1d3c56dae363a15ed18523ffa4367413fd562
Rifqi Nabil Al Fath <alfathnabil26@gmail.com>
2026-03-01 01:55:41 +0700

    fix error

--- src/services/sheetsAuth2.js
+++ src/services/sheetsAuth2.js
@@ -5,8 +5,12 @@ const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 let auth = null;
 
 try {
-  if (process.env.GOOGLE_CREDENTIALS_JADWAL) {
-    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JADWAL);
+  if (process.env.GOOGLE_CREDENTIALS_BASE64_JADWAL) {
+    const credentials = JSON.parse(
+      Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64_JADWAL, "base64").toString(
+        "utf-8",
+      ),
+    );
 
     auth = new google.auth.GoogleAuth({
       credentials,
